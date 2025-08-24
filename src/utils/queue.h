2b0fc537a1abe5d7287c7ae6cb1c133466b61bef
Syed Huzaif <syedhuzaif199@gmail.com>
2025-08-24 23:08:04 +0530

    Enhance queue implementation with error handling and add peek functions - Add macro checks to enable or disable error logging in queue.h - Add peek functions to peek the start or end of the queue

--- src/utils/queue.h
+++ src/utils/queue.h
@@ -14,6 +14,8 @@ typedef struct {
 } Queue;
 Queue *queue_create(size_t data_size, size_t capacity);
 bool enqueue(Queue* queue, void *data);
+void *peek_start(Queue *queue);
+void *peek_end(Queue *queue);
 void *dequeue(Queue *queue);
 void queue_free(Queue *queue);
 typedef void (*queue_data_printer)(Queue *queue);
@@ -26,11 +28,15 @@ void print_queue(Queue *q, queue_data_printer data_printer);
 Queue *queue_create(size_t data_size, size_t capacity) {
     Queue *q = (Queue *)malloc(sizeof(Queue));
     if(!q) {
+        #ifdef QUEUE_H_WARNINGS
         fprintf(stderr, "Error: failed to create queue\n");
+        #endif
     }
     q->data = malloc(data_size * capacity);
     if(!q->data) {
+        #ifdef QUEUE_H_WARNINGS
         fprintf(stderr, "Error: failed to allocate buffer for queue\n");
+        #endif
     }
     q->data_size = data_size;
     q->capacity = capacity;
@@ -41,21 +47,25 @@ Queue *queue_create(size_t data_size, size_t capacity) {
 
 bool enqueue(Queue* queue, void *data) {
     if(queue->end == queue->start) {
+        #ifdef QUEUE_H_WARNINGS
         fprintf(
             stderr,
             "Warning: queue capacity of %zu exceeded! "
             "Make sure that this case is handled by checking if enqueue() returns false.\n",
             queue->capacity
         );
+        #endif
         return false;
     }
     if(queue->start == -1) {
         if(queue->end != 0) {
+        #ifdef QUEUE_H_WARNINGS
             fprintf(
                 stderr,
                 "Error: queue->end != 0 when queue->start == -1:"
                 "when the queue is empty, queue->start == -1 and queue->end == 0\n"
             );
+        #endif
         }
         queue->start = 0;
     }
@@ -66,7 +76,9 @@ bool enqueue(Queue* queue, void *data) {
 }
 void *dequeue(Queue *queue) {
     if(queue->start == -1) {
+        #ifdef QUEUE_H_WARNINGS
         fprintf(stderr, "Warning: trying to dequeue from an empty queue! NULL pointer returned! Make sure the case is handled.\n");
+        #endif
         return NULL;
     }
     void *ret = (void *)((uint8_t *)queue->data + queue->start * queue->data_size);
@@ -77,6 +89,21 @@ void *dequeue(Queue *queue) {
         queue->end = 0;
     }
     return ret;
+}
+
+void *peek_start(Queue *queue) {
+    if(queue->start == -1) {
+        return NULL;
+    }
+    return (void *)((uint8_t *)queue->data + queue->start * queue->data_size);
+}
+
+void *peek_end(Queue *queue) {
+    if(queue->start == -1) {
+        return NULL;
+    }
+
+    return (void *)((uint8_t *)queue->data + queue->end * queue->data_size);
 }
 
 void queue_free(Queue *queue) {
